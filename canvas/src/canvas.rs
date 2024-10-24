225427893fb8659299db1da46b054c08d18b0aae
wba6 <wba6@uakron.edu>
2024-10-23 23:09:15 -0400

    Add getters and setters

--- canvas/src/canvas.rs
+++ canvas/src/canvas.rs
@@ -1,8 +1,16 @@
 use math::color::Color;
 
-pub struct Canvas {}
+pub struct Canvas {
+    height: i32,
+    width: i32
+}
 
 impl Canvas {
+
+    pub fn new(height: i32, width: i32) -> Self {
+        Self { height, width }
+    }
+    
     pub fn write_pixel(&self, x: i32, y: i32, color: Color) {
         panic!("Function not implemented");
     }
@@ -10,4 +18,21 @@ impl Canvas {
         panic!("Function not implemented");
         Color::new(0.0, 0.0, 0.0);
     }
+
+    pub fn height(&self) -> i32 {
+        self.height
+    }
+
+    pub fn width(&self) -> i32 {
+        self.width
+    }
+
+    pub fn set_height(&mut self, height: i32) {
+        self.height = height;
+    }
+
+    pub fn set_width(&mut self, width: i32) {
+        self.width = width;
+    }
+
 }
