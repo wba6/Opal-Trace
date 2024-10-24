1339ff98ba36403939b40aea3d5a85d997b5bda0
wba6 <wba6@uakron.edu>
2024-10-23 23:07:35 -0400

    Format Code

--- canvas/src/canvas.rs
+++ canvas/src/canvas.rs
@@ -3,9 +3,11 @@ use math::color::Color;
 pub struct Canvas {}
 
 impl Canvas {
-    pub fn write_pixel(&self, x: i32, y: i32, color: Color) {panic!("Function not implemented")}
+    pub fn write_pixel(&self, x: i32, y: i32, color: Color) {
+        panic!("Function not implemented");
+    }
     pub fn get_pixel(&self, x: i32, y: i32) -> Color {
+        panic!("Function not implemented");
         Color::new(0.0, 0.0, 0.0);
-        panic!("Function not implemented")
     }
 }
